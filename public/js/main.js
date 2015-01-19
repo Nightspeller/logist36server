ead9e89385d1be2c86eb68d54aa39d1c2acc388f
Serj <serj.nights@live.com>
2015-01-19 16:52:02 +0300

    educationRegistration

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,7 @@ $('document').ready(function(){
     loginAndLogoutButtonHandlers();
 
     registrationButtonsHandler();
+    educationRegistrationButtonsHandler();
 });
 
 function submitFormBothCases(){
@@ -223,6 +224,30 @@ function registrationButtonsHandler(){
             var formData = new FormData($(this)[0]);
             $.ajax({
                 url: '/registration',
+                data: formData,
+                processData: false,
+                contentType: false,
+                type: 'POST',
+                success: function(data){
+                    alert('Заявка отправлена!');
+                    console.log('Success: ', data)
+                },
+                error: function(error){
+                    alert('К сожалению произошла ошибка. Повторите попытку или обратитесь к администратору.');
+                    console.log('Error: ', error)
+                }
+            });
+        })
+    }
+}
+
+function educationRegistrationButtonsHandler(){
+    if (window.location.pathname === '/educationRegistration'){
+        $('#regNewParticipant').submit(function(event){
+            event.preventDefault();
+            var formData = new FormData($(this)[0]);
+            $.ajax({
+                url: '/educationRegistration',
                 data: formData,
                 processData: false,
                 contentType: false,
